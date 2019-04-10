8c82d65ebb87988609a0fc42cb859de537a928e5
JinHui <fifa2004cn@hotmail.com>
2019-04-10 14:20:57 +0800

    new

--- testyoudao.py
+++ testyoudao.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
+from time import sleep
+
 driver = webdriver.Chrome()
-driver.get("http://www.youdao.com")
+driver.get('http://www.youdao.com')
+
 driver.find_element_by_id('translateContent').send_keys('hello')
-#提交输入框的内容
 driver.find_element_by_id('translateContent').submit()
+
+sleep(2)
 driver.quit()
